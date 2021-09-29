f9b2cee3910cb13253fc7017120000c417481d7f
Rafia Zafar <rafiazafar75@gmail.com>
2021-09-29 20:42:18 +0500

    adding

--- src/to-do-Items/TodoItem.js
+++ src/to-do-Items/TodoItem.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
+import todosData from "./todosData";
 
 import "./TodoItem.css";
 
+
+
 function TodoItem(props) {
+
+    const [checked, setChecked] = useState(props.item.completed);
+
+
+       
+
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input  type="checkbox"  checked={props.item.completed} onChange={handleClick} />
       <p>{props.item.text}</p>
     </div>
   );
--- src/to-do-list/List.js
+++ src/to-do-list/List.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import TodoItem from "../to-do-Items/TodoItem";
 import todosData from "../to-do-Items/todosData";
 
 import "./List.css";
 
 function List() {
+
+
+
   const todosItems = todosData.map((item) => (
     <TodoItem key={item.id} item={item} />
   ));
+
   return (
     <div className="container">
       <div className="container-flex">
@@ -15,7 +19,8 @@ function List() {
           <h1> To-do List</h1>
         </div>
 
-        <div className="list-contaier">{todosItems}</div>
+        <div className="list-contaier" >{todosItems}</div>
+        <div className=""></div>
       </div>
     </div>
   );
